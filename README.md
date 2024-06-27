82d9a9587be16ac4bdcb4aed0f64d38e72083a08
AlekUp24 <alekp24@gmail.com>
2024-06-27 13:37:54 +0200

    bootstrap templates, css and js added. Static path done'

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ Ecommerce Website with Django
 Technology:
 
 Backend  -  Django
+
 CSS  -  bootstrap (with bootstrap templates)
+
 DB  -  sqlite3
+
 JS  -  boostrap templates
--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -107,7 +107,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
 STATICFILES_DIRS = ['static/']
 
 MEDIA_URL = 'media/'
