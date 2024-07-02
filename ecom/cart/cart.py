81993f205bd0ef995cc3af8b071422887fea75d6
AlekUp24 <alekp24@gmail.com>
2024-07-02 14:50:53 +0200

    cart summary page

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -1,3 +1,5 @@
+from store.models import Product 
+
 class Cart():
     def __init__(self,request):
 
@@ -7,7 +9,7 @@ class Cart():
         cart = self.session.get('session_key')
 
         # if the user is new
-        if 'key' not in request.session:
+        if 'session_key' not in request.session:
             cart = self.session['session_key'] = {}
 
         # make sure cart is available on all pages
@@ -22,6 +24,17 @@ class Cart():
             pass
         else:
             self.cart[product_id] = {
-                'price': str(product.price),
+                'price': str(product.price)
                 }
         self.session.modified = True
+
+
+    def __len__(self):
+        return len(self.cart)
+    
+
+    def get_products(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in = product_ids)
+
+        return products
--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -8,7 +8,14 @@ from django.http import JsonResponse
 
 
 def cart_summary(request):
-    return render(request, 'cart_summary.html', {})
+
+    cart = Cart(request)
+    cart_products = cart.get_products()
+
+
+    return render(request, 'cart_summary.html', {
+        'cart_products': cart_products,
+        })
 
 def cart_add(request):
     cart = Cart(request)
@@ -17,8 +24,12 @@ def cart_add(request):
         product = get_object_or_404(Product, id=product_id)
         # save to session
         cart.add(product=product)
+        
+        cart_quantity = cart.__len__()
+
         # return response
-        response = JsonResponse({'Product Name: ': product.name})
+        # response = JsonResponse({'Product Name: ': product.name})
+        response = JsonResponse({'qty': cart_quantity})
 
         return response
     
