1299a7274445ef2a0c65a62f572ae0efd1d7405d
AlekUp24 <alekp24@gmail.com>
2024-07-04 13:00:53 +0200

    cart total summary with slaes included

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -70,3 +70,22 @@ class Cart():
         thing = self.cart
 
         return thing
+    
+    def cart_total(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in = product_ids)
+        quantities = self.cart
+
+        total = 0
+
+        for key,value in quantities.items():
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    if product.is_sale:
+                        total = total + (product.sale_price * int(value))
+                    else:
+                        total = total + (product.price * int(value))
+        return total
+
+
