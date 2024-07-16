7fac50b7b1d7a8380eae7aca72ae1cbc8e4d53e1
AlekUp24 <alekp24@gmail.com>
2024-07-16 15:08:33 +0200

    ready to deploy

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -13,7 +13,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY_HO')
+SECRET_KEY = 'django-insecure-nvbeaa&+t_qz9eu86*bx_b*4$)7ida64fnr_5l*6pcxqk^4^x('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
