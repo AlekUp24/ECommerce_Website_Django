8142e1b474703287c783d10911e53da59d258e88
AlekUp24 <alekp24@gmail.com>
2024-07-16 15:03:54 +0200

    domain added

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': os.environ.get('DB_PASSWORD_HO'),
+        'PASSWORD': os.environ['DB_PASSWORD_HO'],
         'HOST': 'roundhouse.proxy.rlwy.net',
         'PORT': '47120',
     }
